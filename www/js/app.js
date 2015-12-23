8f39c1fd051c640add36d72a4295aeff6c3242f5
Phung <dr.rio0904@gmail.com>
2015-12-24 02:01:32 +0700

    Add 1st code

--- www/js/app.js
+++ www/js/app.js
@@ -3,55 +3,69 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('MainApp', ['ionic','MainApp.controllers', 'ngCordova'])
+var db = null
+angular.module('MainApp', ['ionic', 'MainApp.controllers', 'ngCordova'])
 
-    .run(function ($ionicPlatform) {
-        $ionicPlatform.ready(function () {
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform, $cordovaSQLite) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
+        cordova.plugins.Keyboard.disableScroll(true)
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault()
+      }
 
-                // Don't remove this line unless you know what you are doing. It stops the viewport
-                // from snapping when text inputs are focused. Ionic handles this internally for
-                // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
-            }
-            if (window.StatusBar) {
-                StatusBar.styleDefault();
-            }
-        });
+      if (window.sqlitePlugin) {
+        window.plugins.sqlDB.copy('database.db', function () {
+          console.log('PhungHV@Database correctly added')
+          db = $cordovaSQLite.openDB("database.db")
+        }, function (e) {
+          console.log('PhungHV@ Cannot add database |' + e +"|end")
+           db = $cordovaSQLite.openDB("database.db");
+        })
+      }
+
     })
+  })
 
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-            .state('app', {
-                url: '/app',
-                abstract: true,
-                templateUrl: 'templates/menu.html'
-            })
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html'
+      })
 
-            .state('app.playlists', {
-                url: '/playlists',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/playlists.html',
-                        controller: 'PlaylistsCtrl'
-                    }
-                }
-            })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html',
+            controller: 'SearchCtrl'
+          }
+        }
+      })
 
-            .state('app.single', {
-                url: '/playlists/:playlistId',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/playlist.html',
-                        controller: 'PlaylistCtrl'
-                    }
-                }
-            });
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/playlists/');
-    });
-
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      })
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/playlists/')
+  })
--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,15 +1,54 @@
 
 angular.module('MainApp.controllers', ['ionic', 'ngCordova'])
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ]
+  })
+  .controller('SearchCtrl', function ($scope, $cordovaSQLite) {
+    $scope.data ="chua co gi"
+    $scope.insert = function () {
+         $scope.data ="LiST: \n"
+         $scope += $scope.text;
+   var query = 'SELECT song_content FROM SongTbl WHERE song_id = ?'
+      $cordovaSQLite.execute(db, query,$scope.text).then(function (res) {
+        if (res.rows.length > 0) {
+          console.log("Query : ")
+          $scope.data +=res.rows.item(0).song_content
+          console.log('SELECTED -> ' + res.rows.item(0).song_content)
+        } else {
+          console.log('No results found')
+          $scope.data +="\nno result\n"
+        }
+      }, function (err) {
+        console.error(err)
+      })
+    }
 
-    .controller('PlaylistsCtrl', function ($scope) {
-        $scope.playlists = [
-            { title: 'Reggae', id: 1 },
-            { title: 'Chill', id: 2 },
-            { title: 'Dubstep', id: 3 },
-            { title: 'Indie', id: 4 },
-            { title: 'Rap', id: 5 },
-            { title: 'Cowbell', id: 6 }
-        ];
-    })
-
-    .controller('PlaylistCtrl', function ($scope, $stateParams) { });
+    $scope.select = function () {
+        $scope.data ="LiST: \n"
+      var query = 'SELECT song_content FROM SongTbl WHERE song_id = ?'
+      $cordovaSQLite.execute(db, query,'1').then(function (res) {
+        if (res.rows.length > 0) {
+          console.log("Query : ")
+          $scope.data +=res.rows.item(0).song_content
+          console.log('SELECTED -> ' + res.rows.item(0).song_content)
+        } else {
+          console.log('No results found')
+          $scope.data +="\nno result\n"
+        }
+      }, function (err) {
+        console.error(err)
+      })
+    }
+    //Function delete database
+    $scope.delete = function(){
+        $scope.data =": "
+    }
+  })
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {})
